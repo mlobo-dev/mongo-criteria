17d864d60a8aacbf382fbbb2db08c0f63ce5d544
Márcio Lôbo <cast.mlobo.utic@sebrae.com.br>
2022-03-17 10:35:27 -0300

    creating project

---
+++ src/test/java/com/dev4abyss/mongocriteria/MongoCriteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.mongocriteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoCriteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
